1361f1e36c35b9813ccda64212e87227979a230d
Rayn Yorobe <raynyorobe@gmail.com>
2026-03-14 14:19:20 -0500

    fix: Correct syntax for home-manager.sharedModules assignment in flake.nix

--- flake.nix
+++ flake.nix
@@ -45,12 +45,12 @@
             
             home-manager.users.xoviax = import ./modules/home/home.nix;
 
-            home-manager.sharedModules [
+            home-manager.sharedModules = [
               {
                 stylix.targets.btop.colors.enable = true;
               }
-            ]
-            
+            ];
+
             home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
